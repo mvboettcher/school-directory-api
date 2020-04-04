dcf6b0bf6e82797597f6d6cf115c7e0954c6651b
mvboettcher <mvboettcher@gmail.com>
2020-04-04 15:34:29 -0400

    creates express server & connects it to mongoDB instance

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' })
+
+// Connect to DB
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+
+// Logger
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.grey.bold
+  )
+)
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+
+  server.close(() => process.exit(1))
+})
