24f810836b005b9780d60f750223de4d46ad062f
mvboettcher <mvboettcher@gmail.com>
2020-04-05 20:36:44 -0400

    adds birthdate to UserSchema

--- models/User.js
+++ models/User.js
@@ -4,26 +4,30 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Please add a name'],
+    required: [true, 'Please add a name']
   },
   email: {
     type: String,
     match: [
       /^\w+([\.-]?\w+)*@\w+([/.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
+      'Please add a valid email'
     ],
-    required: [true, 'Please add a valid email'],
+    required: [true, 'Please add a valid email']
   },
   sex: {
     type: String,
     enum: ['male', 'female'],
-    required: [true, 'Please add a sex'],
+    required: [true, 'Please add a sex']
   },
   type: {
     type: String,
     enum: ['administration', 'teacher', 'student'],
-    required: [true, 'Please add a type'],
+    required: [true, 'Please add a type']
   },
+  birthdate: {
+    type: String,
+    required: [true, 'Please add a birthdate ( MM/DD/YYYY )']
+  }
 })
 
 module.exports = mongoose.model('User', UserSchema)
